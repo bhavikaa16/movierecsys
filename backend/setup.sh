d8e78cf628122a59782c5445cc0f26a3a6c34db5
Bhavika Vishnoi <bhavikavishnoi@gmail.com>
2025-08-26 22:28:21 +0530

    updated set.sh

--- backend/setup.sh
+++ backend/setup.sh
@@ -4,9 +4,9 @@ set -e
 echo "Downloading model files from Google Drive..."
 
 # Movies.pkl
-gdown --id 1IbK6BH11YOCi5VYKp7US4-uWJIqrf55i -O backend/movies.pkl
+gdown --id 1IbK6BH11YOCi5VYKp7US4-uWJIqrf55i -O movies.pkl
 
 # Similarity.pkl
-gdown --id 1pie8Yhz23tRIiWGyNUGiBxCpeRA_LZIQ -O backend/similarity.pkl
+gdown --id 1pie8Yhz23tRIiWGyNUGiBxCpeRA_LZIQ -O similarity.pkl
 
 echo "Download complete!"
